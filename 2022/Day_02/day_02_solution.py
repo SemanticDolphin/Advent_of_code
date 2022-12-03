e914b4b3355baad42d62f6403ba2bb8f2d832372
SemanticDolphin <e.andersen@tuta.io>
2022-12-03 19:04:44 +0100

    Cleaning up Day 2 solution file

--- 2022/Day_02/day_02_solution.py
+++ 2022/Day_02/day_02_solution.py
@@ -1,81 +1,23 @@
-data = open("data.txt").read().split("\n")[:-1]  # Returns a list of all the games
+data = open("data.txt").read().splitlines()  # Returns a list of all the games
+# Super opaque and I have little clue why it works.
+# TODO: Write an explanation.
 
-
-# ---
-# Part 1
-# ---
-
-
-def game_score(input_game):
-    shapes = input_game.split(" ")
-    score = (ord(shapes[0]) + ord(shapes[1]) + ord(shapes[0]) + 2) % 3 * 3
-    return score
-
-
-def shape_score(input_game):
-    shapes = input_game.split(" ")
-    score = ord(shapes[1]) % 4 + 1
-    return score
-
-
-def combined_score(input_game):
+def game_score_p1(input_game):
     shapes = input_game.split(" ")
     shape_score = ord(shapes[1]) % 4 + 1
     game_score = (ord(shapes[0]) + ord(shapes[1]) + ord(shapes[0]) + 2) % 3 * 3
     return shape_score + game_score
 
 
-def total_score(data):
-    score = 0
-    for game in data:
-        if len(game) > 1:
-            score += game_score(game) + shape_score(game)
-    return score
+def game_score_p2(input_game):
+    shapes = input_game.split(" ")
+    shape_score = (ord(shapes[0]) + ord(shapes[1]) - 1) % 3 + 1
+    game_score = (ord(shapes[1]) - 1) % 3 * 3
+    return shape_score + game_score
 
 
-score = sum([(game_score(game) + shape_score(game)) for game in data])
-combi_score = sum([combined_score(game) for game in data])
-combi_score_compact = sum(
-    [
-        (
-            (ord(g.split(" ")[1]) % 4 + 1)
-            + (
-                (ord(g.split(" ")[0]) + ord(g.split(" ")[1]) + ord(g.split(" ")[0]) + 2)
-                % 3
-                * 3
-            )
-        )
-        for g in data
-    ]
-)
-print(score)
-print(combi_score)
-print(combi_score_compact)
-print(total_score(data))
-# Result: 14827
-# ---
-# Part 2
-# ---
+part_1_solution = sum([game_score_p1(game) for game in data])  # 14827
+part_2_solution = sum([game_score_p2(game) for game in data])  # 13889
 
-
-# def game_score(letter):
-#     # The last letter
-#     return (ord(letter) - 1) % 3 * 3
-#
-#
-# def shape_score(pair_of_letters):
-#     letter = pair_of_letters.split(" ")
-#     return (ord(letter[0]) + ord(letter[1]) - 1) % 3 + 1
-#
-#
-# def total_score(data):
-#     total = 0
-#     for game in data:
-#         if len(game) > 1:
-#             gs = game_score(game.split(" ")[1])
-#             ss = shape_score(game)
-#             total += gs + ss
-#     return total
-#
-#
-# print(total_score(data))
+print(f"Part 1: Total score is {part_1_solution}")
+print(f"Part 2: Total score is {part_2_solution}")
